eb426d58d9f0498aedc22876d7c6ac8b2ed50be0
Skye <928016560@qq.com>
2017-04-25 10:20:34 +0800

    Initial commit

---
+++ src/main/java/com/teamid/TicketBookingBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.teamid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingBackEndApplication.class, args);
+	}
+}
