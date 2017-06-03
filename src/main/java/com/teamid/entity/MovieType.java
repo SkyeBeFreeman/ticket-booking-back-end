a9ea811104351c2921e41478683ecddc643434b2
Weichen Chen <splendidsky1117@gmail.com>
2017-06-03 11:42:03 +0800

    redefine MovieType

--- src/main/java/com/teamid/entity/MovieType.java
+++ src/main/java/com/teamid/entity/MovieType.java
@@ -3,16 +3,18 @@ package com.teamid.entity;
 /**
  * Created by Skye on 2017/6/3.
  */
-public enum MovieType {
-    ACTION,
-    ADVENTURE,
-    COMEDY,
-    CRIME,
-    DRAMA,
-    EPICS,
-    HORROR,
-    MUSICALS,
-    WAR,
-    ANIMATION,
-    PORN
+public class MovieType {
+
+    public static int ACTION = 1;
+    public static int ADVENTURE = 2;
+    public static int COMEDY = 4;
+    public static int CRIME = 8;
+    public static int DRAMA = 16;
+    public static int EPICS = 32;
+    public static int HORROR = 64;
+    public static int MUSICALS = 128;
+    public static int WAR = 256;
+    public static int ANIMATION = 512;
+    public static int PORN = 1024;
+
 }
