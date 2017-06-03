9135a5591b4234626ab9d68d3ef5318512ff291c
wangzf <zefengsysu@gmail.com>
2017-06-03 18:06:32 +0800

    初步完成cinema、movie相关的repository、controller实现

--- src/main/java/com/teamid/repository/CinemaRepository.java
+++ src/main/java/com/teamid/repository/CinemaRepository.java
@@ -1,10 +1,22 @@
 package com.teamid.repository;
 
 import com.teamid.entity.Cinema;
+import com.teamid.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * Created by Skye on 2017/6/3.
  */
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
+
+    List<Cinema> findAll();
+
+    Cinema findOne(Long cinemaId);
+
+
+    Cinema save(Cinema cinema);
+
+    List<Cinema> save(List<Cinema> cinemas);
 }
