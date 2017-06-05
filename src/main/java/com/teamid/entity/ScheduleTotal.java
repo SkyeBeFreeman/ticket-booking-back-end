4789fa58564912212646ec6cae320dba3080be44
Skye <928016560@qq.com>
2017-06-05 13:27:49 +0800

    fix ScheduleTotal cannot be converted bug and Separate a data handler class

--- src/main/java/com/teamid/entity/ScheduleTotal.java
+++ src/main/java/com/teamid/entity/ScheduleTotal.java
@@ -16,7 +16,7 @@ public class ScheduleTotal {
 
     private int hall;
 
-    private LocalDateTime startTime;
+    private String startTime;
 
     private String endTime;
 
@@ -29,9 +29,76 @@ public class ScheduleTotal {
         this.cinemaId = schedule.getCinemaId();
         this.movieId = schedule.getMovieId();
         this.hall = schedule.getHall();
-        this.startTime = schedule.getStartTime();
+        this.startTime = schedule.getStartTime().getMonthValue() + "月"
+                       + schedule.getStartTime().getDayOfMonth() + "日 "
+                       + schedule.getStartTime().getHour() + ":"
+                       + schedule.getStartTime().getMinute();
         this.endTime = schedule.getEndTime();
         this.price = schedule.getPrice();
+        this.tickets = tickets;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getCinemaId() {
+        return cinemaId;
+    }
+
+    public void setCinemaId(long cinemaId) {
+        this.cinemaId = cinemaId;
+    }
+
+    public long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(long movieId) {
+        this.movieId = movieId;
+    }
+
+    public int getHall() {
+        return hall;
+    }
+
+    public void setHall(int hall) {
+        this.hall = hall;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
         this.tickets = tickets;
     }
 }
