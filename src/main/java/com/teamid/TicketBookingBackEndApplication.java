4b5bd3b4c5ac41d0ae060559e9fc33494698aeea
Skye <928016560@qq.com>
2017-06-05 14:17:11 +0800

    fix some bugs

--- src/main/java/com/teamid/TicketBookingBackEndApplication.java
+++ src/main/java/com/teamid/TicketBookingBackEndApplication.java
@@ -1,14 +1,9 @@
 package com.teamid;
 
-import com.teamid.entity.Cinema;
-import com.teamid.entity.Movie;
-import com.teamid.entity.Schedule;
-import com.teamid.entity.Ticket;
 import com.teamid.repository.CinemaRepository;
 import com.teamid.repository.MovieRepository;
 import com.teamid.repository.ScheduleRepository;
 import com.teamid.repository.TicketRepository;
-import com.teamid.utils.DataUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -17,7 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 @EnableRedisHttpSession
--- src/main/java/com/teamid/service/UserServiceImpl.java
+++ src/main/java/com/teamid/service/UserServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import javax.validation.constraints.Null;
 
 /**
  * Created by Skye on 2017/6/3.
