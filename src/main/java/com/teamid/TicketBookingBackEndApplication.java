f88f6380522292fea4e44430515d8983266bef39
Skye <928016560@qq.com>
2017-06-05 14:18:12 +0800

    fix some bugs

--- src/main/java/com/teamid/TicketBookingBackEndApplication.java
+++ src/main/java/com/teamid/TicketBookingBackEndApplication.java
@@ -4,8 +4,6 @@ import com.teamid.repository.CinemaRepository;
 import com.teamid.repository.MovieRepository;
 import com.teamid.repository.ScheduleRepository;
 import com.teamid.repository.TicketRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,10 +14,6 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @SpringBootApplication
 @EnableRedisHttpSession
 public class TicketBookingBackEndApplication {
-
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(TicketBookingBackEndApplication.class);
