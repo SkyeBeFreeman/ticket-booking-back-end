fc6e206829e87763e87276ad094ca8609f5ff897
Skye <928016560@qq.com>
2017-06-08 17:54:46 +0800

    添加档期过期后不显示档期

--- src/main/java/com/teamid/service/ScheduleServiceImpl.java
+++ src/main/java/com/teamid/service/ScheduleServiceImpl.java
@@ -2,12 +2,15 @@ package com.teamid.service;
 
 import com.teamid.dao.ScheduleDAO;
 import com.teamid.entity.Schedule;
+import com.teamid.utils.LocalDateTimeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Created by Skye on 2017/6/3.
@@ -32,6 +35,8 @@ public class ScheduleServiceImpl implements ScheduleService {
 
     @Override
     public List<Schedule> findSchedulesByCinemaId(long cinemaId) {
-        return scheduleDAO.findSchedulesByCinemaId(cinemaId);
+        return scheduleDAO.findSchedulesByCinemaId(cinemaId).stream()
+                .filter(i -> LocalDateTimeUtils.getDifference(i.getStartTime(), LocalDateTime.now()) >= 0)
+                .collect(Collectors.toList());
     }
 }
